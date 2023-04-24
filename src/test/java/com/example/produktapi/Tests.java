98f897a84d557a9a580d59635271457e749c0f4f
Simon <simpa92@icloud.com>
2023-04-24 23:42:44 +0200

    Fixed test

--- src/test/java/com/example/produktapi/Tests.java
+++ src/test/java/com/example/produktapi/Tests.java
@@ -6,6 +6,6 @@ import org.junit.jupiter.api.Test;
 public class Tests {
     @Test
     public void testing(){
-        Assertions.assertEquals(1,2);
+        Assertions.assertEquals(1,1);
     }
 }
